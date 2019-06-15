b3edf70e8598c576017ea2859a168505e7ed523b
Pavel Macenauer <macenauer.p@gmail.com>
2019-06-15 11:47:27 +0200

    Added closing price to record lagging span

--- cryptobot/data_analysis/data_collector.py
+++ cryptobot/data_analysis/data_collector.py
@@ -1,5 +1,6 @@
 import sqlite3
 from dateutil.parser import parse
+from datetime import timedelta
 import threading
 from time import sleep
 from cryptobot.hitbtc.hitbtc_client import NoDataException
@@ -25,14 +26,19 @@ class DataCollector(object):
     UPDATE_QUERY = "UPDATE MinMax_{3} SET min=(CASE WHEN min>{1} THEN {1} ELSE min END), " \
                    "max=(CASE WHEN max>{2} THEN max ELSE {2} END) WHERE date='{0}'"
 
-    CREATE_QUERY = "CREATE TABLE IF NOT EXISTS MinMax_{0} " \
-                   "(date DATETIME PRIMARY KEY, min FLOAT, max FLOAT, UNIQUE(date, min, max) ON CONFLICT REPLACE)"
+    UPDATE_CLOSING_PRICE = "UPDATE MinMax_{2} SET closing_price={1} WHERE date='{0}'"
 
-    SELECT_MINMAX_QUERY = (
-        "SELECT date, min, max FROM MinMax_{0} " 
-        "WHERE datetime(date) >= datetime('now', '-{1} minute') "
+    CREATE_QUERY = "CREATE TABLE IF NOT EXISTS MinMax_{0} " \
+                   "(date DATETIME PRIMARY KEY, min FLOAT, max FLOAT, closing_price FLOAT DEFAULT NULL, " \
+                   "UNIQUE(date, min, max, closing_price) ON CONFLICT REPLACE)"
+
+    SELECT_MINMAX_QUERY = "SELECT date, min, max FROM MinMax_{0} " \
+        "WHERE datetime(date) >= datetime('now', '-{1} minute') " \
         "ORDER BY date ASC"
-    )
+
+    SELECT_CLOSING_PRICE_QUERY = "SELECT date, closing_price FROM MinMax_{0} " \
+        "WHERE datetime(date) >= datetime('now', '-{1} minute') " \
+        "ORDER BY date ASC LIMIT {2}"
 
     DB_NAME = 'my_db.db'
 
@@ -58,6 +64,9 @@ class DataCollector(object):
         for c in candles:
             date = parse(c['timestamp'])
             cursor.execute(self.REPLACE_QUERY.format(date, c['min'], c['max'], self.__currency))
+        for c in candles:
+            prev_date = parse(c['timestamp']) - timedelta(minutes=1)
+            cursor.execute(self.UPDATE_CLOSING_PRICE.format(prev_date, c['close'], self.__currency))
 
     def initialize_candles(self):
         conn = sqlite3.connect(self.DB_NAME)
@@ -87,6 +96,9 @@ class DataCollector(object):
                     cursor.execute(self.UPDATE_QUERY.format(date, c['min'], c['max'], self.__currency))
                     print("Updating entry (date: {0}, min: {1}, max: {2})".format(date, c['min'], c['max']))
                 else:
+                    prev_date = date - timedelta(minutes=1)
+                    cursor.execute(self.UPDATE_CLOSING_PRICE.format(prev_date, c['close'], self.__currency))
+                    print("Updating closing price (date: {0}, close: {1})".format(prev_date, c['close']))
                     cursor.execute(self.REPLACE_QUERY.format(date, c['min'], c['max'], self.__currency))
                     print("Adding entry (date: {0}, min: {1}, max: {2})".format(date, c['min'], c['max']))
             conn.commit()
@@ -145,3 +157,24 @@ class DataCollector(object):
         if not vals:
             raise NoDataException
         return vals
+
+    def get_closing_prices_over_time(self, period_in_min: int, offset_in_min: int) -> list:
+        """Returns a list of closing prices for a given period of time with an offset in the past (all in minutes).
+        E.g. if period_in_min=20 and offset_in_min=10, then it means 20 values (1 per minute) ranging from 30 minutes to
+        10 minutes in the past.
+
+        Args:
+            period_in_min: number of minutes to collect data for
+            offset_in_min: minute shift to the past
+
+        Returns:
+             A list of values containing closing prices.
+        """
+        conn = sqlite3.connect(self.DB_NAME)
+        cursor = conn.cursor()
+
+        query = self.SELECT_MINMAX_QUERY.format(self.__currency, period_in_min + offset_in_min, period_in_min)
+        rows = cursor.execute(query).fetchall()
+        conn.close()
+
+        return rows
--- cryptobot/plot_figure.py
+++ cryptobot/plot_figure.py
@@ -13,35 +13,39 @@ if __name__ == "__main__":
                           config['hitbtc_credentials']['public_key'],
                           config['hitbtc_credentials']['secret'])
 
-    r = client.create_buy_order(symbol="ADABTC", quantity=100, price=0.00002)
-    print(r)
-    r = client.create_sell_order(symbol="ADABTC", quantity=100, price=0.002)
-    print(r)
-
-    '''dc = DataCollector(client)
+    dc = DataCollector(client)
 
     num_of_minutes = 12 * 60
 
     while True:
         conversion_line, base_line, leading_span_A, leading_span_B = [], [], [], []
 
+        period_len = 10.0 / 9.0
+
+        short_period = int(9 * period_len)
+        mid_period = int(26 * period_len)
+        long_period = int(52 * period_len)
+
         try:
-            for i in dc.get_highlow_over_time(num_of_minutes, 10):
+            for i in dc.get_highlow_over_time(num_of_minutes, short_period):
                 conversion_line.append((i['min'] + i['max']) / 2.0)
 
-            for i in dc.get_highlow_over_time(num_of_minutes, 30):
+            for i in dc.get_highlow_over_time(num_of_minutes, mid_period):
                 base_line.append((i['min'] + i['max']) / 2.0)
 
             x = range(len(conversion_line))
             for i in x:
                 leading_span_A.append((conversion_line[i] + base_line[i]) / 2.0)
 
-            for i in dc.get_highlow_over_time(num_of_minutes, 120):
+            for i in dc.get_highlow_over_time(num_of_minutes, long_period):
                 leading_span_B.append((i['min'] + i['max']) / 2.0)
+
+            #lagging_span = dc.get_closing_price(num_of_minutes, mid_period)
 
             plt.plot(x, conversion_line)
             plt.plot(x, base_line)
             plt.plot(x, leading_span_A, x, leading_span_B)
+            #plt.plot(x, lagging_span)
 
             a = np.array(leading_span_A)
             b = np.array(leading_span_B)
@@ -59,4 +63,4 @@ if __name__ == "__main__":
             print("Failed to receive data... current time is {0}".format(datetime.now()))
             sleep(1)
 
-    plt.show()'''
+    plt.show()
